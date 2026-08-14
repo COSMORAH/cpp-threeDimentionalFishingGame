d5a5b5bdff2a4ecd6dc35d6d92572f92b695983a
theme33alt <github.daniel.gryga33@protonmail.com>
2026-08-14 17:00:11 +0100

    did stuff

--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,38 @@ int main() {
 	raylib::Color textColor = raylib::Color::DarkGray();
 	raylib::Window window(screenWidth, screenHeight, "Fishing Game");
 	SetTargetFPS(60);
+    // DisableCursor();
 
-	// Main game loop
+    // Define the camera to look into our 3d world
+    Camera3D camera = { 0 };
+    camera.position = (Vector3){ 0.0f, 10.0f, 10.0f };  // Camera position
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 45.0f;                                // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type
+
+    Vector3 worldCenter = { 0.0f, 0.0f, 0.0f };
+
+    // Main game loop
     while (!window.ShouldClose()) {   // Detect window close button or ESC key
-        // Update
-        //----------------------------------------------------------------------------------
-        // Update your variables here
-        //----------------------------------------------------------------------------------
+        // update variables
+        UpdateCamera(&camera, CAMERA_FREE);
 
-        // Draw
-        //----------------------------------------------------------------------------------
-        while (window.Drawing()) {
-        }
-        //----------------------------------------------------------------------------------
+        // draw everything
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            BeginMode3D(camera);
+
+                DrawCube(worldCenter, 2.0f, 2.0f, 2.0f, RED);
+                DrawCubeWires(worldCenter, 2.0f, 2.0f, 2.0f, MAROON);
+
+                DrawGrid(10, 1.0f);
+
+            EndMode3D();
+        
+        EndDrawing();
     }
 
 	return 0;
@@ -27,7 +46,5 @@ int main() {
 
 /*
 ##### INFORMATION FOR LATER #####
-
-textColor.DrawText("text", [x axis], [y axis], [scale]);
 
 */
